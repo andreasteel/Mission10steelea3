a58186ea9d0ce439112298e03c42248b73fe8b4d
andreasteel <102710199+andreasteel@users.noreply.github.com>
2023-03-09 10:19:40 -0700

    fix

--- Pages/Buy.cshtml.cs
+++ Pages/Buy.cshtml.cs
@@ -4,13 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mission9steelea3.Models;
 
 namespace Mission9steelea3.Pages
 {
     public class BuyModel : PageModel
     {
+        private IBookstoreRepository repo { get; set; }
+
+        public BuyModel (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public Basket basket { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost(string title)
+        {
+            Book b = repo.Books.FirstOrDefault(x => x.Title == title);
+
+            basket = new Basket();
+            basket.AddItem(b, 1);
+
+            return RedirectToPage();
         }
     }
 }
